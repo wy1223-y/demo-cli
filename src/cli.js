7d21067f7675af97702e05e49d1babcd2aa26ac1
Kyle <kevindsp@outlook.com>
2021-10-20 23:13:31 +0800

    feat: 添加命令解析

--- src/cli.js
+++ src/cli.js
@@ -1,5 +1,15 @@
-// src/cli.js
-module.exports = function cli() {
-  // 打印命令参数
-  console.log(process.argv);
-};
+const { Command } = require("commander");
+const pkg = require("../package.json");
+
+const program = new Command();
+
+program.version(pkg.version, "-v, --version", "output version");
+
+program
+  .command("create <name>")
+  .description("根据模板名创建仓库")
+  .action((name) => {
+    console.log(name);
+  });
+
+program.parse(process.argv);
