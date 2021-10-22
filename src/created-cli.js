d74c3062e088057cdcc55be1e339f45769b6309a
王艳 <wangyan@yinhechuangxiang.com>
2021-10-22 20:31:39 +0800

    feat:new-cli

--- src/created-cli.js
+++ src/created-cli.js
@@ -7,7 +7,7 @@ program.version(pkg.version, "-v, --version", "output version");
 
 program
   .command("yc <name>")
-  .description("创建一个文件")
+  .description("打印name")
   .option('-n, --name <name> [smallName]', '王艳', '考拉')
   .action((name, option) => {
     console.log(name, option);
